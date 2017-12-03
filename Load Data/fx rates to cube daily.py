186fdaad85ef710a598a6aedae814b0b51bc8aba
Marius Wirtz <MariusWirtz2@gmail.com>
2017-12-03 09:50:10 +0100

    Time Series Forecasting
    - New Time Series Forecasting Sample in Jupyter Notebook

---
+++ Load Data/fx rates to cube daily.py
@@ -0,0 +1,71 @@
+"""
+Read FX data from FRED (Federal Reserve of St. Louis data) through pandas and push it to the fx cube
+
+Prerequisites:
+1. Create the required cubes and dimensions
+    Run TM1py sample Load Data\sample setup.py
+2. Install pandas
+    type 'pip install pandas' into cmd if you don't have pandas installed
+3. Add pandas_reader module:
+    To install it, run in command line: pip install pandas_datareader
+
+"""
+
+from datetime import datetime
+# type 'pip install pandas_datareader' into cmd if you don't have pandas_datareader installed
+import pandas_datareader.data as web
+
+from TM1py.Services import TM1Service
+
+
+# Connection parameters
+address = "localhost"
+port = 12354
+user = "admin"
+password = "apple"
+ssl = True
+
+# FX Cube Name
+cube_name = 'TM1py FX Rates'
+
+# FRED Tickers with properties
+# https://fred.stlouisfed.org/series/DEXUSEU
+currency_pairs = {
+            'DEXUSAL': {'From': 'USD', 'To': 'AUD', 'Invert': 'N'},
+            'DEXUSUK': {'From': 'USD', 'To': 'GBP', 'Invert': 'Y'},
+            'DEXUSEU': {'From': 'USD', 'To': 'EUR', 'Invert': 'Y'},
+            'DEXHKUS': {'From': 'USD', 'To': 'HKD', 'Invert': 'N'},
+            'DEXSIUS': {'From': 'USD', 'To': 'SGD', 'Invert': 'N'},
+            'DEXSZUS': {'From': 'USD', 'To': 'CHF', 'Invert': 'N'},
+            'DEXCHUS': {'From': 'USD', 'To': 'RMB', 'Invert': 'N'},
+            'DEXTAUS': {'From': 'USD', 'To': 'TWD', 'Invert': 'N'},
+            'DEXINUS': {'From': 'USD', 'To': 'INR', 'Invert': 'N'},
+            'DEXJPUS': {'From': 'USD', 'To': 'JPY', 'Invert': 'N'}
+}
+
+# Container to store data that we write to TM1 in the end (in one batch)
+cellset = dict()
+
+for currency_ticker, currency_details in currency_pairs.items():
+    # load FX Rates for USD to JPY from FRED through pandas datareader
+    start = datetime(year=2016, month=1, day=1)
+    raw_data = web.get_data_fred(currency_ticker, start)
+
+    # Remove NaN
+    data = raw_data.dropna()
+
+    # Push values into cellset
+    for tmstp, data in data.iterrows():
+        date = tmstp.date()
+        # Handle Inverted FX Rates
+        if currency_details["Invert"] == 'Y':
+            value = 1 / data.values[0]
+        else:
+            value = data.values[0]
+        coordinates = (currency_details["From"], currency_details["To"], str(date), 'Spot')
+        cellset[coordinates] = value
+
+with TM1Service(address=address, port=port, user=user, password=password, ssl=ssl) as tm1:
+    tm1.cubes.cells.write_values(cube_name, cellset)
+
+
--- Other/cleanup model.py
+++ Other/cleanup model.py
@@ -16,37 +16,37 @@ with TM1Service(address='localhost', port=12354, user='admin', password='apple',
     regex_list = ['^temp_*', '^test*', '^TM1py*']
 
     # Iterate through cubes
-    cubes = tm1.cubes.get_all_names()
-    for cube in cubes:
+    cube_names = tm1.cubes.get_all_names()
+    for cube_name in cube_names:
         for regex in regex_list:
-            if re.match(regex, cube, re.IGNORECASE):
-                tm1.cubes.delete(cube)
+            if re.match(regex, cube_name, re.IGNORECASE):
+                tm1.cubes.delete(cube_name)
                 break
             else:
-                private_views, public_views = tm1.cubes.views.get_all(cube_name=cube)
-                for view in private_views:
-                    if re.match(regex, view.name, re.IGNORECASE):
-                        tm1.cubes.views.delete(cube_name=cube, view_name=view.name, private=True)
-                for view in public_views:
-                    if re.match(regex, view.name, re.IGNORECASE):
-                        tm1.cubes.views.delete(cube_name=cube, view_name=view.name, private=False)
+                private_view_names, public_views_names = tm1.cubes.views.get_all_names(cube_name=cube_name)
+                for view_name in private_view_names:
+                    if re.match(regex, view_name, re.IGNORECASE):
+                        tm1.cubes.views.delete(cube_name=cube_name, view_name=view_name, private=True)
+                for view_name in private_view_names:
+                    if re.match(regex, view_name, re.IGNORECASE):
+                        tm1.cubes.views.delete(cube_name=cube_name, view_name=view_name, private=False)
 
-    # Get Dimension names. Filter out }ElementAttributes and }Hierarchies Dimensions
-    dimensions = [dimension for dimension in tm1.dimensions.get_all_names()
-                  if not dimension.startswith('}ElementAttributes_') and not dimension.startswith('}Hierarchies_')]
+    # Get Dimension names. Filter out Control Dimensions
+    dimension_names = [dimension for dimension in tm1.dimensions.get_all_names() if not dimension.startswith('}')]
     # Iterate through dimensions
-    for dimension in dimensions:
+    for dimension_name in dimension_names:
         for regex in regex_list:
-            if re.match(regex, dimension, re.IGNORECASE):
-                tm1.dimensions.delete(dimension)
+            if re.match(regex, dimension_name, re.IGNORECASE):
+                tm1.dimensions.delete(dimension_name)
                 break
             else:
                 # Iterate through public subsets
-                subsets = tm1.dimensions.subsets.get_all_names(dimension_name=dimension, hierarchy_name=dimension,
+                subsets = tm1.dimensions.subsets.get_all_names(dimension_name=dimension_name,
+                                                               hierarchy_name=dimension_name,
                                                                private=False)
                 for subset in subsets:
                     if re.match(regex, subset, re.IGNORECASE):
-                        tm1.dimensions.subsets.delete(dimension_name=dimension, subset_name=subset, private=False)
+                        tm1.dimensions.subsets.delete(dimension_name=dimension_name, subset_name=subset, private=False)
 
     # Iterate through Processes
     processes = tm1.processes.get_all_names()
