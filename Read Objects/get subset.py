7b69029542be93cfa4b6d333ed86f5032d99d9b8
vviau <vviau@cubewise.com>
2018-04-22 16:20:45 +0200

    Add config.ini to Read Objects samples

--- Read Objects/get subset.py
+++ Read Objects/get subset.py
@@ -1,27 +1,33 @@
 """
 Get private and public Subsets from TM1
 """
+import configparser
+config = configparser.ConfigParser()
+config.read('..\config.ini')
 
 from TM1py.Services import TM1Service
 
-with TM1Service(address='localhost', port=12354, user='admin', password='apple', ssl=True) as tm1:
+dimension_name='Region'
+hierarchy_name='Region'
 
-    private_subsets = tm1.dimensions.subsets.get_all_names(dimension_name='plan_department',
-                                                           hierarchy_name='plan_department',
+with TM1Service(**config['tm1srv01']) as tm1:
+
+    private_subsets = tm1.dimensions.subsets.get_all_names(dimension_name=dimension_name,
+                                                           hierarchy_name=hierarchy_name,
                                                            private=True)
     print('private subsets: ')
     for subset_name in private_subsets:
-        subset = tm1.dimensions.subsets.get(dimension_name='plan_department',
+        subset = tm1.dimensions.subsets.get(dimension_name=dimension_name,
                                             subset_name=subset_name,
                                             private=True)
         print(subset.name)
 
-    public_subsets = tm1.dimensions.subsets.get_all_names(dimension_name='plan_department',
-                                                          hierarchy_name='plan_department',
+    public_subsets = tm1.dimensions.subsets.get_all_names(dimension_name=dimension_name,
+                                                          hierarchy_name=hierarchy_name,
                                                           private=False)
     print('public subsets: ')
     for subset_name in public_subsets:
-        subset = tm1.dimensions.subsets.get(dimension_name='plan_department',
+        subset = tm1.dimensions.subsets.get(dimension_name=dimension_name,
                                             subset_name=subset_name,
                                             private=False)
         print(subset.name)
