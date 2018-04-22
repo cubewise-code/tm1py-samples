53ee6e9eabbb8c2249b21fd7919c83968d900e26
vviau <vviau@cubewise.com>
2018-04-22 11:43:14 +0200

    add config.ini to run TI code.py

--- Other/run TI code.py
+++ Other/run TI code.py
@@ -1,11 +1,13 @@
 """
 Run loose statements of TI
 """
-
+import configparser
+config = configparser.ConfigParser()
+config.read('..\config.ini')
 
 from TM1py.Services import TM1Service
 
-with TM1Service(address='localhost', port=12354, user='admin', password='apple', ssl=True) as tm1:
+with TM1Service(**config['tm1srv01']) as tm1:
 
     # Sample 1
     ti_statements = [
