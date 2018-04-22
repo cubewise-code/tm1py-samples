d7ec25fab140b53087050905443fed9a08974d22
vviau <vviau@cubewise.com>
2018-04-22 08:54:37 +0200

    Add config.ini to generate mdx from native view.py

--- Other/generate mdx from native view.py
+++ Other/generate mdx from native view.py
@@ -2,14 +2,21 @@
 Load existing cube view from TM1 into python. Then ask TM1py to generate the MDX Query from the cube view
 """
 
+import configparser
+config = configparser.ConfigParser()
+config.read('..\config.ini')
+
 from TM1py.Services import TM1Service
 
+cube = 'General Ledger'
+view = 'P&L'
 
 # Establish connection to TM1 Server
-with TM1Service(address='localhost', port=12354, user='admin', password='apple', ssl=True) as tm1:
+with TM1Service(**config['source']) as tm1:
 
     # Instantiate TM1py.NativeView object
-    nv = tm1.cubes.views.get_native_view('Plan_BudgetPlan', 'High Level Profit And Loss', private=False)
+    nv = tm1.cubes.views.get_native_view(cube, view, private=False)
 
     # Retrieve MDX from native view. Print it
     print(nv.MDX)
+
