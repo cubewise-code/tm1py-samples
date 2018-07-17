776b090b483108154d8bc1e6f72591ced8e6102a
Marius Wirtz <MariusWirtz2@gmail.com>
2018-07-17 23:57:14 +0200

    Refactoring - mdx_query_different_formats

--- Query Data/mdx_query_different_formats.py
+++ Query Data/mdx_query_different_formats.py
@@ -1,16 +1,21 @@
 """
 Query a default TM1 "Planning Sample" server and show results in different formats
 """
+
+import pprint
 import configparser
+
+from TM1py.Services import TM1Service
+from TM1py.Utils import Utils
+
+# Read Config
 config = configparser.ConfigParser()
 config.read('..\config.ini')
 
-import pprint
-import json
-import inspect
+# Initializer Prettyprinter
+pp = pprint.PrettyPrinter(indent=2)
 
-from TM1py.Services import TM1Service
-
+# Connect to TM1
 with TM1Service(**config['tm1srv01']) as tm1:
 
     mdx = """
@@ -25,35 +30,39 @@ with TM1Service(**config['tm1srv01']) as tm1:
             [plan_report].[Budget]
             )
         """
-    mdx3 = """
-        SELECT
-        NON EMPTY {[plan_time].[Q1-2004],[plan_time].[Q2-2004],[plan_time].[Q3-2004],[plan_time].[Q4-2004]} on COLUMNS,
-        NON EMPTY {[plan_chart_of_accounts].[Revenue],[plan_chart_of_accounts].[Net Operating Income]} on ROWS,
-        NON EMPTY {[plan_business_unit].[Europe],[plan_business_unit].[North America] } on PAGES
-        FROM [plan_Report]
-        WHERE (
-            [plan_department].[Total Organization],
-            [plan_exchange_rates].[actual],
-            [plan_report].[Budget]
-            )
-        """
 
-    pp = pprint.PrettyPrinter(indent=2)
+    # raw: native response dictionary
+    raw_data = tm1.cubes.cells.execute_mdx_raw(
+        mdx=mdx,
+        cell_properties=["Value", "RuleDerived"],
+        elem_properties=["Type"],
+        top=None)
+    pp.pprint(raw_data)
 
-    for query in [mdx,mdx3]:
-        print('MDX Query:')
-        print(query)
-        # Create view content
-        cellset_id = tm1.cubes.cells.create_cellset(query)
-        try:
+    # default: (Case and space insensitive) dictionary
+    data = tm1.cubes.cells.execute_mdx(mdx)
+    print(data)
+    # Note: can be converted into pandas dataframe with Utility function:
+    df = Utils.build_pandas_dataframe_from_cellset(data, multiindex=False)
+    pp.pprint(df)
 
-            for output_format in ['raw', 'default', 'values', 'csv', 'dataframe', 'array', 'dygraph']:
-                result = tm1.cubes.cells.extract_cellset(cellset_id, result_format=output_format, value_precision=2)
-                print("{} output format".format(output_format))
-                pp.pprint( list(result) if inspect.isgenerator(result) else result )
-                print()
+    # values: get only the cell values
+    values = tm1.cubes.cells.execute_mdx_values(mdx)
+    for value in values:
+        print(value)
 
-        finally:
-            tm1.cubes.cells.delete_cellset(cellset_id)
+    # csv: get cellset content as raw csv (Zero / null is suppressed by default, Context dimensions are omitted)
+    csv = tm1.cubes.cells.execute_mdx_csv(mdx)
+    print(csv)
 
-        print()
+    # dataframe: get cellset content as pandas dataframe (Zero / null is suppressed by default, Context dimensions are omitted)
+    df = tm1.cubes.cells.execute_mdx_dataframe(mdx)
+    print(df)
+
+    # array
+    array = tm1.cubes.cells.execute_mdx_ui_array(mdx, value_precision=1)
+    pp.pprint(array)
+
+    # dygraph
+    dygraph = tm1.cubes.cells.execute_mdx_ui_dygraph(mdx, value_precision=1)
+    pp.pprint(array)
