0af4181a3fb6981d38cab8b75190477f0dc88576
Marius Wirtz <MariusWirtz2@gmail.com>
2019-02-20 23:07:30 +0100

    Samples for new get_default_member and update_default_member methods

---
+++ Other/update_default_member.py
@@ -0,0 +1,21 @@
+"""
+When a dimension of a cube is not explicitly referenced in an MDX Query,
+TM1 will implicitly use the DefaultMember for the missing dimension.
+If no DefaultMember is defined in TM1, it will use the element with index 1.
+
+You can use TM1py to query and update the default member for a Hierarchy
+"""
+
+import configparser
+
+from TM1py.Services import TM1Service
+
+config = configparser.ConfigParser()
+config.read('..\config.ini')
+
+with TM1Service(**config['tm1srv02']) as tm1:
+    new_default_member = "2019-01-01"
+    tm1.dimensions.hierarchies.update_default_member(
+        dimension_name="Date",
+        hierarchy_name="Date",
+        member_name=new_default_member)
