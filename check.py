22ea52ce7ba3411308cb9717887a1869fe5d2216
Marius Wirtz <MariusWirtz2@gmail.com>
2019-04-02 22:20:37 +0200

    Enhance check.py script to support sso
    Now check.py can be used to check connection against TM1 using SSO

--- check.py
+++ check.py
@@ -7,16 +7,27 @@ from distutils.util import strtobool
 from TM1py.Services import TM1Service
 
 # Parameters for connection
-user = input("TM1 User: ")
-password = getpass.getpass("Password (cmd doesn't show input): ")
+user = input("TM1 User (leave empty if SSO): ")
+password = getpass.getpass("Password (cmd doesn't show input, leave empty if SSO): ")
 namespace = input("CAM Namespace (leave empty if no CAM Security): ")
 address = input("Address (leave empty if localhost): ")
-port = input("Port: ")
+gateway = input("ClientCAMURI (leave empty if no SSO): ")
+port = input("HTTP Port: ")
 ssl = strtobool(input("SSL (True or False): "))
 
 if len(namespace.strip()) == 0:
     namespace = None
 
-with TM1Service(address=address, port=port, user=user, password=password, namespace=namespace, ssl=ssl) as tm1:
+if len(gateway.strip()) == 0:
+    gateway = None
+
+with TM1Service(
+        address=address,
+        port=port,
+        user=user,
+        password=password,
+        namespace=namespace,
+        gateway=gateway,
+        ssl=ssl) as tm1:
     server_name = tm1.server.get_server_name()
     print("Connection to TM1 established!! your Servername is: {}".format(server_name))
